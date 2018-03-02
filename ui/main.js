27ada4763ccdc2d7b25833488ab75845a957dbf7
Javed Khan <javedalikhan50@gmail.com>
2018-03-03 00:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('madi');
-madi.onclick= function () {
-    img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick= function () {
+    var interval = setInterval(moveRight, 50);
+    //img.style.marginLeft = '100px';
 };
