dd36ee11152165b0d94726c4a524e6e45fe6893c
Javed Khan <javedalikhan50@gmail.com>
2018-03-03 01:56:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,13 +49,11 @@ submit.onclick = function (){
     // make a request to server and the send the name
     
     //capture a list of name and render it as a list
-    var name = ['name1', 'name2', 'name3'];
+    var names = ['name1', 'name2', 'name3'];
     var list = '';
     for (var i=0; i< names.length; i++){
-        list += '<li>' +name[i] + '<li>';
+        list += '<li>' +names[i] + '<li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
-    }
-    
-}
+};
