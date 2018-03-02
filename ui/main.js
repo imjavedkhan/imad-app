327a59e24dca1d4d232a7f19262d2adda42a24d1
Javed Khan <javedalikhan50@gmail.com>
2018-03-03 01:21:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,30 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 //change the text of the main-text div
-var element = document.getElementById('main-text');
+//var element = document.getElementById('main-text');
 
-element.innerHTML = 'New value';
+//element.innerHTML = 'New value';
 
 //move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick= function () {
-    var interval = setInterval(moveRight, 50);
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight(){
+  //  marginLeft = marginLeft + 1;
+    //img.style.marginLeft = marginLeft + 'px';
+//}
+//img.onclick= function () {
+  //  var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';
+//};
+
+var button = document.getElementById('counter');
+button.onclick = function (){
+    //make a request to the counter endpoint
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the coorect span
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();    
 };
