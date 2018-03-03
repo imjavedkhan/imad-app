13da62c42c35c5219534186b1871431b7f4c3afb
Javed Khan <javedalikhan50@gmail.com>
2018-03-03 09:55:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,13 +47,29 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
     // make a request to server and the send the name
+     //create a request object
+    var request = new XMLHttpRequest();
     
-    //capture a list of name and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '<li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                //capture a list of name and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length; i++){
+                    list += '<li>' + names[i] + '<li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        //not done yet
+     };
+     // make the request
+     request.open('GET', 'http://javedalikhan50.imad.hasura-app.io/submit-name?name=' + name, true);
+     request.send(null);
+    
 };
