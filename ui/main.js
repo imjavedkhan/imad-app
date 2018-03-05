5f2dcae5d0c9726de327f735960016c774f41b0a
Javed Khan <javedalikhan50@gmail.com>
2018-03-05 23:00:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,6 +31,7 @@ submit.onclick = function (){
     console.log(username);
     console.log(password);
      request.open('POST', 'http://javedalikhan50.imad.hasura-app.io/login', true);
+     request.setRequestHeader('Content-Type', 'application/json');
      request.send(JSON.stringify({username: username, password: password}));
     
 };
