e5f88601998396ec4fa10911487e0398e85bfd35
Javed Khan <javedalikhan50@gmail.com>
2018-03-05 22:58:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,46 +1,5 @@
-//console.log('Loaded!');
 
-//change the text of the main-text div
-//var element = document.getElementById('main-text');
-
-//element.innerHTML = 'New value';
-
-//move the image
-//var img = document.getElementById('madi');
-//var marginLeft = 0;
-//function moveRight(){
-  //  marginLeft = marginLeft + 1;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick= function () {
-  //  var interval = setInterval(moveRight, 50);
-    //img.style.marginLeft = '100px';
-//};
-
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function (){
-    //create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();    
-            }
-        }
-        //not done yet
-     };
-     // make the request
-     request.open('GET', 'http://javedalikhan50.imad.hasura-app.io/counter', true);
-     request.send(null);
-    
-};
-
+//Submit username/password to login
 //submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
@@ -54,22 +13,24 @@ submit.onclick = function (){
             //take some action
             if(request.status === 200){
                 //capture a list of name and render it as a list
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++){
-                    list += '<li>' + names[i] + '<li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+               console.log('user logged in');
+               alert('logged in successfully');
+            }else if (request.status === 403){
+                alert('username/password is incorrect');
+            } else if (request.status === 500){
+                alert('Something went wrong on the server');
+            }
+                
             }
         }
         //not done yet
      };
      // make the request
-     var nameInput = document.getElementById('name');
-        var name = nameInput.value;
-     request.open('GET', 'http://javedalikhan50.imad.hasura-app.io/submit-name?name=' + name, true);
-     request.send(null);
+     var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+     request.open('POST', 'http://javedalikhan50.imad.hasura-app.io/login', true);
+     request.send(JSON.stringify({username: username, password: password}));
     
 };
