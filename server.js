37aabd2bdcc02d7ab83af3acaaee5e9398fef426
Javed Khan <javedalikhan50@gmail.com>
2018-03-05 23:14:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'javedalikhan50',
@@ -16,7 +17,11 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret: 'someRandomSecretvalue',
+    cookie: { maxAge: 1000*60*60*24*30
+    }
+}));
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
@@ -102,6 +107,9 @@ app.post('/login', function (req, res){
             var salt = dbString.split('$')[2];
             var hashedPassword = hash(password, salt);
             if (hashedPassword === dbString){
+                //set the session
+                req.session.auth = {userId: result.rows[0].id};                
+                
                  res.send('credential correct');
             }else{
                 res.send(403),send('username/password is invalid');
@@ -110,6 +118,14 @@ app.post('/login', function (req, res){
        
     } 
    });
+});
+
+app.get('/check-login', function (req,res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('You are logged in:' + req.session.auth.userId.toString());
+   } else{
+       res.send(403).send('username/password is invalid');
+   }
 });
 
 var pool = new Pool(config);
